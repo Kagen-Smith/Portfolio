3261fd139140e5c48c2bf9fe10781832e006fd12
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-04 01:25:28 -0600

    added more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-
+import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 import Header from '../src/components/Header.jsx';
 import Footer from '../src/components/Footer.jsx';
-import React from 'react';
 
-class App {
-  constructor(props) {
-    super(props);
-    this.state = { matches: window.matchMedia("(min-width: 800px)").matches };
-    const handler = e => this.setState({ matches: e.matches });
-    window.matchMedia("(min-width: 800px)").addEventListener('change', handler);
-  }
-
-
-
-  render() {
-    return (
-      <>
+function App() {
+  
+  return (
+    <div>
+      <div>
         <Header />
+      </div>
+      <div>
         <Outlet />
+   </div>
+      <div>
         <Footer />
-      </>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
 export default App;

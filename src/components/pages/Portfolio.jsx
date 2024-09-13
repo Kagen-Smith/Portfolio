5069298c5e3e25daefc60c73ab24d3b693352c57
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-09-13 01:57:16 -0600

    finished Portfolio css, had to add a new file called project which creates a template of how the css will work on the portfolio.jsx file

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,42 +1,45 @@
 import '/./src/components/styles/portfolio.css';
 import project1 from '/src/assets/project1.png'
 import project2 from '/src/assets/project2.png'
+import Project from '/src/components/pages/project.jsx'
 
-function Portfolio() {
-    const state = [
-        {
-            name: 'Music Wizard',
-            image: project1,
-            description: 'A website that can create a playlist from a preselceted list of songs.',
-            link: 'https://kagen-smith.github.io/Project-01-Music-Wizard/',
-            repo: 'https://github.com/Kagen-Smith/Project-01-Music-Wizard/'
-        },
-        {
-            name: 'README.md Generator',
-            image: project2,
-            description: 'this is a command line application that generates a README.md file for a project.',
-            instructionLink: 'https://www.youtube.com/watch?v=bfA92zXLDro',
-            repo: 'https://github.com/Kagen-Smith/nodejs-challenge/'
-        },
-  
-    ]; 
 
+
+
+const project = [
+    {
+        id: 1,
+        image: project1,
+        title: 'Music Wizard',
+        deployedLink: 'https://kagen-smith.github.io/Project-01-Music-Wizard/',
+        githubLink: 'https://github.com/Kagen-Smith/Project-01-Music-Wizard',
+    },
+    {
+        id: 2,
+        image: project2,
+        title: 'README Generator',
+        deployedLink: 'https://youtu.be/bfA92zXLDro?si=d-1AhHKKNitTrjTT',
+        githubLink: 'https://github.com/Kagen-Smith/nodejs-challenge/tree/main',
+    },
+   
+    // Add more projects as needed
+];
+
+export default function Portfolio() {
     return (
-        <section className='portfolio-section'>
-        <div>
-            <h1 className='portfolio-section h1'>Portfolio</h1>
-            {state.map((project, index) => (
-                <div key={index}>
-                    <h3>{project.name}</h3>
-                    <img src={project.image} alt={project.name} className='.project-image' />
-                    <p>{project.description}</p>
-                    <a href={project.link}>Link to the project</a>
-                    <a href={project.repo}>Link to the repo</a>
-                </div>
-            ))}
-        </div>
+        <section className="portfolio-section">
+           <h2>Portfolio</h2>
+           <div className="projects-grid">
+                {project.map(project => (
+                    <Project
+                        key={project.id}
+                        image={project.image}
+                        title={project.title}
+                        deployedLink={project.deployedLink}
+                        githubLink={project.githubLink}
+                    />
+                ))}
+           </div>
         </section>
     );
 }
-
-export default Portfolio;
---
+++ src/components/pages/project.jsx
@@ -0,0 +1,21 @@
+import '/./src/components/styles/portfolio.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+export default function Project({ image, title, deployedLink, githubLink }) {
+    return (
+        <div className="project-container">
+            <div className="project-image">
+                <img src={image} alt={title} />
+                <div className="overlay">
+                    <a href={deployedLink} className="project-title" target="_blank" rel="noopener noreferrer">
+                        {title}
+                    </a>
+                    <a href={githubLink} className="github-link" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
